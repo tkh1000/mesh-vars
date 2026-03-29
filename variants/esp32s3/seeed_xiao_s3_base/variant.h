c9a6b83e6f1ac1685fef49cb1b951c488e59baed
K <mail.krisho@gmail.com>
2026-03-28 20:27:59 -0400

    Refactor LORA and GPS pin definitions in variant.h
    Updated variant.h to redefine LORA and GPS pin configurations.

--- variants/esp32s3/seeed_xiao_s3_base/variant.h
+++ variants/esp32s3/seeed_xiao_s3_base/variant.h
@@ -25,9 +25,8 @@
 */
 
 /*
-Board Information: https://www.seeedstudio.com/XIAO-ESP32S3-Sense-p-5639.html
-Expansion Board Infomation : https://www.seeedstudio.com/Seeeduino-XIAO-Expansion-board-p-4746.html
-L76K GPS Module Information : https://www.seeedstudio.com/L76K-GNSS-Module-for-Seeed-Studio-XIAO-p-5864.html
+Board Information: https://www.seeedstudio.com/XIAO-ESP32S3-p-5627.html=
+Radio Board Information: https://www.seeedstudio.com/Wio-SX1262-for-XIAO-p-6379.html
 */
 
 #define LED_POWER 48
@@ -40,50 +39,54 @@ L76K GPS Module Information : https://www.seeedstudio.com/L76K-GNSS-Module-for-S
 #define ADC_CHANNEL ADC1_GPIO1_CHANNEL
 #define BATTERY_SENSE_RESOLUTION_BITS 12
 
-/*Warning:
-    https://www.seeedstudio.com/L76K-GNSS-Module-for-Seeed-Studio-XIAO-p-5864.html
-    L76K Expansion Board can not directly used, L76K Reset Pin needs to override or physically remove it,
-    otherwise it will conflict with the SPI pins
-*/
-#define GPS_L76K
-#ifdef GPS_L76K
-#define GPS_RX_PIN 44
-#define GPS_TX_PIN 43
-#define HAS_GPS 1
-#define GPS_THREAD_INTERVAL 50
-#define PIN_SERIAL1_RX PIN_GPS_TX
-#define PIN_SERIAL1_TX PIN_GPS_RX
-#define PIN_GPS_STANDBY 1
-#endif
-
-// XIAO S3 Expansion board  has 1.3 inch OLED Screen
-#define USCREEN_SSD1306
-
-#define I2C_SDA 5
-#define I2C_SCL 6
 
 // XIAO S3 LORA module
+#undef LORA_MOSI
+#undef LORA_MISO
+#undef LORA_SCK
+#undef LORA_CS
+#undef LORA_RESET
+#undef LORA_DIO1
+#undef LORA_DIO2
+#undef SX126X_CS
+#undef SX126X_DIO1
+#undef SX126X_BUSY
+#undef SX126X_RESET
+#undef SX126X_DIO2_AS_RF_SWITCH
+#undef SX126X_RXEN
+#undef SX126X_TXEN
+#undef SX126X_DIO3_TCXO_VOLTAGE
+#undef PIN_SPI_MOSI
+#undef PIN_SPI_MISO
+#undef PIN_SPI_SCK
+#undef PIN_SPI_SS
+#undef GPS_RX_PIN
+#undef GPS_TX_PIN
+#undef HAS_GPS
+#undef I2C_SDA
+#undef I2C_SCL
+#undef HAS_WIRE
+
 #define USE_SX1262
-
-#define LORA_MISO 8
-#define LORA_SCK 7
-#define LORA_MOSI 9
-#define LORA_CS 41
-
-#define LORA_RESET 42
-#define LORA_DIO1 39
-
-#define LORA_DIO2 38
-
-#ifdef USE_SX1262
-#define SX126X_CS LORA_CS
-#define SX126X_DIO1 LORA_DIO1
-#define SX126X_BUSY 40
-#define SX126X_RESET LORA_RESET
-
-//  DIO2 controlls an antenna switch and the TCXO voltage is controlled by DIO3
+#define LORA_MOSI        9
+#define LORA_MISO        8
+#define LORA_SCK         7
+#define LORA_DIO1        2
+#define LORA_RESET       3
+#define LORA_CS          5
+#define SX126X_CS        5
+#define SX126X_DIO1      2
+#define SX126X_BUSY      4
+#define SX126X_RESET     3
 #define SX126X_DIO2_AS_RF_SWITCH
-#define SX126X_RXEN 38
-#define SX126X_TXEN RADIOLIB_NC
+#define SX126X_RXEN      6
+#define SX126X_TXEN      RADIOLIB_NC
 #define SX126X_DIO3_TCXO_VOLTAGE 1.8
+#define PIN_SPI_MOSI     9
+#define PIN_SPI_MISO     8
+#define PIN_SPI_SCK      7
+#define PIN_SPI_SS       5
+#define HAS_GPS          0
+#define HAS_WIRE         0
+
 #endif
