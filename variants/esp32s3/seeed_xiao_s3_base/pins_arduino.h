666f1434c2984a4542b063fb23c68b2e25e9706f
K <mail.krisho@gmail.com>
2026-03-28 20:22:57 -0400

    Update SS pin mapping from 41 to 5

--- variants/esp32s3/seeed_xiao_s3_base/pins_arduino.h
+++ variants/esp32s3/seeed_xiao_s3_base/pins_arduino.h
@@ -8,14 +8,13 @@
 
 // GPIO48 Reference: https://github.com/espressif/arduino-esp32/pull/8600
 
-// The default Wire will be mapped to Screen and Sensors
-static const uint8_t SDA = 47;
-static const uint8_t SCL = 48;
 
 // Default SPI will be mapped to Radio
 static const uint8_t MISO = 8;
 static const uint8_t SCK = 7;
 static const uint8_t MOSI = 9;
-static const uint8_t SS = 41;
+static const uint8_t SS = 5;
+
+
 
 #endif /* Pins_Arduino_h */
