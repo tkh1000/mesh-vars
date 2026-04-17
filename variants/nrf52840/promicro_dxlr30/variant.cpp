6241653f77fb671e50bc2cedef7ede51b74659d3
K <mail.krisho@gmail.com>
2026-04-17 18:02:47 -0400

    Implement LED initialization and shutdown function
    Added initialization for LED and shutdown function.

--- variants/nrf52840/promicro_dxlr30/variant.cpp
+++ variants/nrf52840/promicro_dxlr30/variant.cpp
@@ -1,20 +1,19 @@
 /*
  * variant.cpp - NRF52840 Pro Micro + DXLR30 / E22-900M22S (SX1262)
-   *
-   * Copyright (c) 2014-2015 Arduino LLC. All right reserved.
-   * Copyright (c) 2016 Sandeep Mistry All right reserved.
+ *
+ * Copyright (c) 2014-2015 Arduino LLC. All right reserved.
+ * Copyright (c) 2016 Sandeep Mistry All right reserved.
  * Copyright (c) 2018, Adafruit Industries (adafruit.com)
  *
  * LGPL 2.1 or later
  */
 
 #include "variant.h"
+#include "nrf.h"
 #include "wiring_constants.h"
 #include "wiring_digital.h"
-#include "nrf.h"
 
-// Pin description table for NRF52840 Pro Micro
-// Flat index 0-47 maps to NRF52840 GPIO.
+// Flat pin map: index 0-47 -> NRF52840 GPIO
 // P0.xx = index xx,  P1.xx = index 32+xx
 const uint32_t g_ADigitalPinMap[] = {
   // P0 port
@@ -26,3 +25,12 @@ const uint32_t g_ADigitalPinMap[] = {
   32,  33,  34,  35,  36,  37,  38,  39,
   40,  41,  42,  43,  44,  45,  46,  47
 };
+
+void initVariant()
+{
+  // Turn off the LED at boot
+  pinMode(PIN_LED1, OUTPUT);
+  digitalWrite(PIN_LED1, HIGH); // Active LOW - HIGH = off
+}
+
+void variant_shutdown() {}
